e924c683b31864988b3eb6dbd9520e897a95f14d
Tobenna <ezetobenna@gmail.com>
2022-11-11 20:01:17 -0600

    Update with Comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//Packages imported
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:avatar_glow/avatar_glow.dart';
@@ -9,12 +10,12 @@ import 'package:geocoding/geocoding.dart';
 import 'package:restart_app/restart_app.dart';
 import 'dart:async';
 
-
+//Main function to run the application
 void main() {
   runApp(MyApp());
 }
 
-
+//Stateless class function to build the application
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
+//Stateful widget
 class SpeechScreen extends StatefulWidget {
 
   @override
@@ -38,27 +39,34 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
+  //Declaration of object for Translation
   GoogleTranslator translator = GoogleTranslator();
   FlutterTts tts = FlutterTts();
 
-
+  //Declaration of langCode as an empty string
   String langCode = "";
 
+  //Asynchronous function for Translation
   translate() async{
+    //Translator object is called to translate input(_text) from listen function
     await translator.translate(_text, to: langCode).then((output) {
+      //State is set on the translated input and the output is converted to a string
       setState(() {
         _text = output.toString();
       });
     });
   }
 
+  //Asynchronous function for the Text-to-speech action done on translated text
+  //Pitch of the speech is set and the Text-to-Speech package produces the translated speech
   speak() async{
     await tts.setLanguage(langCode);
     await tts.setPitch(1.0);
     await tts.speak(_text);
   }
-
+  //Function for highlighting certain words
   final Map<String, HighlightedWord> _highlights = {
+    //When the application encounters the word 'flutter', it will be highlighted
     'flutter': HighlightedWord(
       onTap: () => print('flutter'),
       textStyle: const TextStyle(
@@ -69,11 +77,19 @@ class _SpeechScreenState extends State<SpeechScreen> {
     ),
   };
 
+  //Speech to text object declaration
   late stt.SpeechToText _speech;
+
+  //_isListening is initialized to false until the listen app is called
   bool _isListening = false;
+
+  //Placeholder text for the application display box
   String _text = 'Press the button and start speaking';
+
+  //
   double _confidence = 1.0;
 
+  //Asynchronous Function to determine location
   getLocation() async{
     LocationPermission permission;
     permission = await Geolocator.checkPermission();
@@ -90,18 +106,20 @@ class _SpeechScreenState extends State<SpeechScreen> {
     print(placemarks[0].locality.toString());
   }
 
+  //Initialization function for initializing states in the application
+  //getLocation, initialListen, _speech are initialized here
   @override
   void initState() {
-    _speech = stt.SpeechToText();
-    getLocation();
-    initialListen();
+    _speech = stt.SpeechToText(); // initialization for _speech object
+    getLocation(); //getLocation initialized on app launch
+    initialListen(); //initialListen initialized on app launch
   }
-
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Whizz!'),
+        title: Text('Whizz!'), //App title bar
         leading: GestureDetector(
           onDoubleTap: () {
                 Restart.restartApp();
@@ -119,15 +137,15 @@ class _SpeechScreenState extends State<SpeechScreen> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            FloatingActionButton(
+            FloatingActionButton( //Floating action button for the listen button
               onPressed: _listen,
               child: Icon(_isListening ? Icons.mic: Icons.mic_none),
             ),
-            FloatingActionButton(
+            FloatingActionButton( //Floating action button for the translate button
               onPressed: (){
-                translate();
-                Future.delayed(Duration(minutes: 0, milliseconds: 800), (){
-                  speak();
+                translate(); //Translate function called to translate input from listen function
+                Future.delayed(Duration(minutes: 0, milliseconds: 800), (){ //output delayed for 8 secs to be able to correctly relay the translated text
+                  speak(); //Speak function called
                 });
               },
               child: Icon(Icons.translate),
@@ -160,18 +178,18 @@ class _SpeechScreenState extends State<SpeechScreen> {
   }
 
 
-
+  // _listen function called when listen button is pressed
   void _listen() async {
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
         onError: (val) => print('onError: $val'),
       );
-      if (available) {
+      if (available) { //if _listening is available, its state is changed to true and the resultant text is checked with the words in  the "recognized words" library
         setState(() => _isListening = true);
         _speech.listen(
           onResult: (val) => setState(() {
-            _text = val.recognizedWords;
+            _text = val.recognizedWords;  //resultant text converted from speech checked with the recognized library
             if (val.hasConfidenceRating && val.confidence > 0) {
               _confidence = val.confidence;
             }
@@ -179,13 +197,12 @@ class _SpeechScreenState extends State<SpeechScreen> {
         );
       }
     } else {
-      setState(() => _isListening = false);
+      setState(() => _isListening = false); //If listening has stopped, _isListening is set as false
       _speech.stop();
     }
   }
 
-  /// Determine the current position of the device.
-
+  // Asynchronous to determine the current position of the device.
   Future<Position> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
@@ -215,7 +232,7 @@ class _SpeechScreenState extends State<SpeechScreen> {
   }
 
   bool listening1 = false;
-
+  //Asynchronous function to detect keyword
   initialListen() async {
     if (!listening1) {
       bool available = await _speech.initialize(
@@ -225,23 +242,29 @@ class _SpeechScreenState extends State<SpeechScreen> {
       if (available) {
         setState(() => listening1 = true);
         _speech.listen(
-          listenFor: Duration(seconds: 40),
+          listenFor: Duration(seconds: 40), //Time added to listen for keyword
           onResult: (val) =>
               setState(() {
                 _text = val.recognizedWords;
+                //Spanish keywords to be detected
                 if (_text.contains("Bien") || _text.contains("Hola") || _text.contains("Buenos dias") || _text.contains("buenos dias") || _text.contains("buenos noches")) {
                   setState((){langCode = "es";
                   print(langCode);});
                 }
+                //French keywords to be detected
                 else if (_text.contains("bonjour") || _text.contains("Bonjour") || _text.contains("Bonsoir") || _text.contains("bonsoir")) {
                   setState((){langCode = "fr";
                   print(langCode);});
-                }  else if (_text.contains("Obrigado")) {
+                }
+                //Portuguese keywords to be detected
+                else if (_text.contains("Obrigado")) {
                   setState(() {
                     langCode = "pt";
                     print(langCode);
                   });
-                }else {
+                }
+                //Default is to English if no keyword detected
+                else {
                   setState((){langCode = "en";
                   print(langCode);});
                 }
